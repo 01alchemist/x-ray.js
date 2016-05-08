5604a0f98c3318c27a3915da74a5f3962b8d807e
Nidin Vinayakan <01@01alchemist.com>
2016-05-08 21:26:38 +0200

    Release 1 missing files

---
+++ modules/xrenderer/workers/trace-worker-bootstrap.js
@@ -0,0 +1,24 @@
+importScripts(
+    "../../../node_modules/systemjs/dist/system.src.js",
+    "../bundle/xrenderer.js"
+);
+
+System.config({
+    packages: {
+        "./": {
+            format: 'register',
+            defaultExtension: 'js'
+        },
+        'xrenderer': {
+            main: "xrenderer",
+            format: 'register',
+            defaultExtension: 'js'
+        }
+    },
+    map: {
+        'xrenderer': "../bundle/"
+    }
+});
+System.import('TraceWorker').then(function(module){
+    //console.log(module);
+}, console.error.bind(console));
